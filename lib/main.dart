bbdf4478e200f654ae625d41bb81c2fbf5a37aa8
Lukk17 <luksarna@gmail.com>
2019-02-08 21:25:09 +0100

    GUI update

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  String _TITLE = "It is your time !";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(
-
+//        in normal ThemeData:
 //        primarySwatch: Colors.red,
-
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+          ),
+      home: MyHomePage(title: _TITLE),
     );
   }
 }
@@ -23,9 +26,10 @@ class MyHomePage extends StatefulWidget {
 
   // This widget is the home page of your application.
   // Fields in a Widget subclass are always marked "final".
-
   final String title;
 
+  // override to manually creates private (starting with _ ) subclass
+  // to update state of counter widget
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -33,6 +37,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  // update state of widget to increase count value
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -44,50 +49,88 @@ class _MyHomePageState extends State<MyHomePage> {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: _createAppBar(),
+          body: _createBody(),
+          floatingActionButton: _createFloatingActionButton()),
+    );
+  }
+
+  MaterialButton _createCategoryButton(String text) {
+    MaterialButton cb = MaterialButton(
+      onPressed: _incrementCounter,
+      height: 60,
+      minWidth: 350,
+      color: Colors.red,
+      child: Text(text),
+    );
+    Timer(Duration(seconds: 20), null);
+    return cb;
+  }
+
+  Container _spacer() {
+    return Container(margin: EdgeInsets.all(5));
+  }
+
+  Widget _createAppBar() {
+    return AppBar(
+      // Here we take the value from the MyHomePage object that was created by
+      // the App.build method, and use it to set our appbar title.
+      title: Text(widget.title),
+      backgroundColor: Colors.red,
+      bottom: TabBar(
+        tabs: <Widget>[Tab(text: "log"), Tab(text: "timer")],
       ),
-      body: Center(
+    );
+  }
+
+  Widget _createBody() {
+    return TabBarView(children: [
+      Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Column(
+              children: <Widget>[
+                _createCategoryButton('chest'),
+                _spacer(),
+                _createCategoryButton('back'),
+                _spacer(),
+                _createCategoryButton('arm'),
+                _spacer(),
+                _createCategoryButton('leg'),
+              ],
+            ),
             Text(
               'You have pushed the button this many times:',
+              style: TextStyle(color: Colors.red),
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              style:
+                  Theme.of(context).textTheme.display1.apply(color: Colors.red),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      Center()
+    ]);
+  }
+
+  Widget _createFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: _incrementCounter,
+
+      // text which will be shown after long press on button
+      tooltip: 'Increment',
+
+      child: Icon(Icons.add),
+      backgroundColor: Colors.red,
+      foregroundColor: Colors.black,
     );
   }
 }
